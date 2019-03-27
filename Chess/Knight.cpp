ae28b291171eab8490f859ba1cc7104239cafab4
Alex Coupe <acoupe@gmail.com>
2019-03-27 16:23:00 +0000

    Knight moves added.

--- Chess/Knight.cpp
+++ Chess/Knight.cpp
@@ -8,4 +8,62 @@ Knight::Knight(int x, int y, const std::string spritename)
 
 void Knight::GetMoves(const Map* mypieces, const Map* opponentpieces)
 {
+	moves.clear();
+
+	int new_x = 1;
+	int new_y = 2;
+
+	//check up
+	if (mypieces->count({ coords.x - new_x,coords.y - new_y }) == 0 && (coords.y - new_y) >= minCoord.y 
+		&& (coords.x - new_x) >= minCoord.x)
+	{
+		moves.push_back({ coords.x - new_x,coords.y - new_y });
+	}
+
+	if (mypieces->count({ coords.x + new_x,coords.y - new_y }) == 0 && (coords.y - new_y) >= minCoord.y
+		&& (coords.x + new_x) <= maxCoord.x)
+	{
+		moves.push_back({ coords.x + new_x,coords.y - new_y });
+	}
+
+	//check down
+	if (mypieces->count({ coords.x - new_x,coords.y + new_y }) == 0 && (coords.y + new_y) <= maxCoord.y
+		&& (coords.x - new_x) >= minCoord.x)
+	{
+		moves.push_back({ coords.x - new_x,coords.y + new_y });
+	}
+
+	if (mypieces->count({ coords.x + new_x,coords.y + new_y }) == 0 && (coords.y + new_y) <= maxCoord.y
+		&& (coords.x + new_x) <= maxCoord.x)
+	{
+		moves.push_back({ coords.x + new_x,coords.y + new_y });
+	}
+
+	//check left
+	new_x = 2;
+	new_y = 1;
+
+	if (mypieces->count({ coords.x - new_x,coords.y + new_y }) == 0 && (coords.y + new_y) <= maxCoord.y
+		&& (coords.x - new_x) >= minCoord.x)
+	{
+		moves.push_back({ coords.x - new_x,coords.y + new_y });
+	}
+
+	if (mypieces->count({ coords.x - new_x,coords.y - new_y }) == 0 && (coords.y - new_y) >= minCoord.y
+		&& (coords.x - new_x) >= minCoord.x)
+	{
+		moves.push_back({ coords.x - new_x,coords.y - new_y });
+	}
+
+	if (mypieces->count({ coords.x + new_x,coords.y + new_y }) == 0 && (coords.y + new_y) <= maxCoord.y
+		&& (coords.x + new_x) <= maxCoord.x)
+	{
+		moves.push_back({ coords.x + new_x,coords.y + new_y });
+	}
+
+	if (mypieces->count({ coords.x + new_x,coords.y - new_y }) == 0 && (coords.y - new_y) >= minCoord.y
+		&& (coords.x + new_x) <= maxCoord.x)
+	{
+		moves.push_back({ coords.x + new_x,coords.y - new_y });
+	}
 }
